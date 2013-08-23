cb813e08ed9aede08cc2d7ecfc674dfbcf49f4ef
YangMann <zhangyang93@rocketmail.com>
2013-08-23 18:38:37 +0800

    card view implementation

--- ChiChi/src/edu/SJTU/ChiChi/views/VerticalTextView.java
+++ ChiChi/src/edu/SJTU/ChiChi/views/VerticalTextView.java
@@ -20,22 +20,18 @@ public class VerticalTextView extends TextView {
 
     public VerticalTextView(Context context) {
         super(context);
-        // TODO Auto-generated constructor stub
     }
 
     public VerticalTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        // TODO Auto-generated constructor stub
     }
 
     public VerticalTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public void setText(CharSequence text, BufferType type) {
-        // TODO Auto-generated method stub
         super.setText(text, type);
     }
 
