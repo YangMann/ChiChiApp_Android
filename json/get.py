9d5a4610029c921a28776ca778f0635037c15aef
YangMann <zhangyang93@rocketmail.com>
2013-08-29 11:01:20 +0800

    json

--- json/get.py
+++ json/get.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import jft
 
 fin = open("chichi.csv", "r")
 fout = open("food.json", "w")
@@ -7,14 +8,14 @@ fout.write('[')
 preb = ""
 prer = ""
 for line in fin.readlines():
-    while line[-1]=='\n' or line[-1]=='\r':
+    while line[-1] == '\n' or line[-1] == '\r':
         line = line[0:-1]
     line = line.replace('\\', '\\\\')
     arr = line.split(';')
     photographer = arr[0]
     name = arr[1]
     restaurant = arr[2]
-    if(restaurant == '哈乐餐厅'):
+    if (restaurant == '哈乐餐厅'):
         building = "哈乐"
     else:
         building = restaurant[0:6]
@@ -29,37 +30,36 @@ for line in fin.readlines():
         fout.write('{'
                    + '"building":"' + building + '",'
                    + '"restaurants":[{'
-                   '"restaurant":"' + restaurant + '",'
+                     '"restaurant":"' + restaurant + '",'
                    + '"foods":['
-                   )
+        )
     else:
         if (prer != restaurant):
             fout.write(']}')
-            if(preb != building):
+            if (preb != building):
                 fout.write(']},'
                            + '{'
                            + '"building":"' + building + '",'
                            + '"restaurants":['
-                           )
+                )
             else:
                 fout.write(',')
             fout.write('{' +
                        '"restaurant":"' + restaurant + '",'
                        + '"foods":['
-                       )
+            )
         else:
             fout.write(',')
 
-
     fout.write('{'
-               + '"name":"' + name + '",'
+               + '"name":"' + jft.j2f("utf8", "utf8", name) + '",'
                + '"url":"' + url + '",'
                + '"genre":"' + genre + '",'
-               + '"price":"' + price + '",'
+               + '"price":"¥' + price + '",'
                + '"taste":"' + taste + '",'
                + '"description":"' + description + '"'
                + '}'
-               )
+    )
     preb = building
     prer = restaurant
 fout.write(']}]}]')
